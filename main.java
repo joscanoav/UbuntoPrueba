98dea5f2a930f5c8b2ae85de834cdd98255cbd63
joscanoav <joscanoav@gmail.com>
2023-10-17 12:09:09 +0200

    main m say goodbye

---
+++ main.java
@@ -0,0 +1,8 @@
+Public void status void main(String[] args){
+	//Saludo
+	System.out.println("Hola Mundo");
+	System.out.println("Vengo del futuro");
+	//Despedida
+	System.out.println("Adios");
+	System.out.printlm("Desde la otra rama");
+}
